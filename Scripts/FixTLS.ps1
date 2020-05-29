bb679833a4caa5a3beb3fa8929c688b5de863a92
Micah Battin <micah.o.battin@gmail.com>
2020-05-29 10:21:40 -0700

    Added FixTLS Script

---
+++ Scripts/FixTLS.ps1
@@ -0,0 +1,4 @@
+﻿Push-Location hklm:
+$RootPaths = Get-ChildItem -Path "HKLM:\SOFTWARE\Microsoft\.NETFramework\v?.*","HKLM:\SOFTWARE\WOW6432Node\Microsoft\.NETFramework\v?.*"
+New-ItemProperty -Path $RootPaths -PropertyType 'dword' -Name 'SchUseStrongCrypto' -Value 1 -Force
+Pop-Location
